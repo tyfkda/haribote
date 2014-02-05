74697412310348219aacf480be1a593dbd228de2
tyfkda <tyfkda@gmail.com>
2014-02-06 06:21:01 +0900

    Write memory to draw image

--- bootpack.c
+++ bootpack.c
@@ -1,7 +1,10 @@
 void io_hlt();
 
 void HariMain(void) {
- fin:
-  io_hlt();
-  goto fin;
+  unsigned char* p = (unsigned char*)0xa0000;
+  for (int i = 0x0000; i <= 0xffff; ++i)
+    p[i] = i & 0x0f;
+
+  for (;;)
+    io_hlt();
 }
