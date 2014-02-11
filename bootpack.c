e5fad8f6cdb8216d84585ce7ab06802eb16a3a16
tyfkda <tyfkda@gmail.com>
2014-02-11 22:23:42 +0900

    Scroll console when enter key pressed

--- bootpack.c
+++ bootpack.c
@@ -93,9 +93,9 @@ void console_task(SHTCTL* shtctl, SHEET* sheet) {
   timer_init(timer, &task->fifo, 1);
   timer_settime(timer, 50);
 
-  int cursor_x = 16, cursor_c = -1;
+  int cursor_x = 16, cursor_y = 28, cursor_c = -1;
   // Show prompt.
-  putfonts8_asc_sht(shtctl, sheet, 8, 28, COL8_WHITE, COL8_BLACK, ">", 1);
+  putfonts8_asc_sht(shtctl, sheet, cursor_x - 8, cursor_y, COL8_WHITE, COL8_BLACK, ">", 1);
 
   for (;;) {
     io_cli();
@@ -106,15 +106,36 @@ void console_task(SHTCTL* shtctl, SHEET* sheet) {
     int i = fifo_get(&task->fifo);
     io_sti();
     if (256 <= i && i < 512) {  // Keyboard data (from task A).
-      if (i == 8 + 256) {  // Back space.
+      switch (i) {
+      case 10 + 256:  // Enter.
+        putfonts8_asc_sht(shtctl, sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, " ", 1);
+        if (cursor_y < 28 + 112) {
+          cursor_y += 16;
+        } else {
+          unsigned char* buf = sheet->buf;
+          int bxsize = sheet->bxsize;
+          // Scroll.
+          for (int y = 28; y < 28 + 112; ++y)
+            for (int x = 8; x < 8 + 240; ++x)
+              buf[x + y * bxsize] = buf[x + (y + 16) * bxsize];
+          // Erase last line.
+          boxfill8(buf, bxsize, COL8_BLACK, 8, 28 + 112, 8 + 240, 28 + 112 + 16);
+          sheet_refresh(shtctl, sheet, 8, 28, 8 + 240, 28 + 128);
+        }
+        cursor_x = 16;
+        // Show prompt.
+        putfonts8_asc_sht(shtctl, sheet, cursor_x - 8, cursor_y, COL8_WHITE, COL8_BLACK, ">", 1);
+        break;
+      case 8 + 256:  // Back space.
         if (cursor_x > 16) {
-          putfonts8_asc_sht(shtctl, sheet, cursor_x, 28, COL8_WHITE, COL8_BLACK, " ", 1);
+          putfonts8_asc_sht(shtctl, sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, " ", 1);
           cursor_x -= 8;
         }
-      } else {  // Normal character.
+        break;
+      default:  // Normal character.
         if (cursor_x < 240) {
           char s[] = { i - 256, '\0' };
-          putfonts8_asc_sht(shtctl, sheet, cursor_x, 28, COL8_WHITE, COL8_BLACK, s, 1);
+          putfonts8_asc_sht(shtctl, sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, s, 1);
           cursor_x += 8;
         }
       }
@@ -129,15 +150,15 @@ void console_task(SHTCTL* shtctl, SHEET* sheet) {
         break;
       case 2:  cursor_c = COL8_WHITE; break;
       case 3:
-        boxfill8(sheet->buf, sheet->bxsize, COL8_BLACK, cursor_x, 28, cursor_x + 8, 44);
+        boxfill8(sheet->buf, sheet->bxsize, COL8_BLACK, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
         cursor_c = -1;
         break;
       }
     }
     // Redraw cursor.
     if (cursor_c >= 0) {
-      boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 8, 44);
-      sheet_refresh(shtctl, sheet, cursor_x, 28, cursor_x + 8, 44);
+      boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
+      sheet_refresh(shtctl, sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
     }
   }
 }
@@ -271,6 +292,10 @@ void HariMain(void) {
       sprintf(s, "%02X", i - 256);
       putfonts8_asc_sht(shtctl, sht_back, 0, 16, COL8_WHITE, COL8_DARK_CYAN, s, 2);
       switch (i) {
+      case 0x1c + 256:  // Enter.
+        if (key_to != 0)  // To console.
+          fifo_put(&task_cons->fifo, 10 + 256);
+        break;
       case 0x0e + 256:  // Back space.
         if (key_to == 0) {  // To task A
           if (cursor_x > 8) {
