217393f4a312b87e09959f9476853c6deab2f7a1
tyfkda <tyfkda@gmail.com>
2014-02-17 05:24:42 +0900

    Make `%x` to handle unsigned int and `%p` to handle pointer in sprintf

--- stdio.c
+++ stdio.c
@@ -1,25 +1,30 @@
 #include "stdio.h"
 
-static char* int2num(char *s, int x, int base, const char* table,
-                     char padding, int keta) {
-  int negative = FALSE;
-  if (x < 0) {
-    x = -x;
-    negative = TRUE;
-  }
+static char* uint2num(char *s, unsigned int x, int base, const char* table,
+                      char padding, int keta) {
   *(--s) = '\0';
   do {
     *(--s) = table[x % base];
     x /= base;
     --keta;
   } while (x > 0);
-  if (negative) {
-    *(--s) = '-';
-    --keta;
-  }
   for (; keta > 0; --keta)
     *(--s) = padding;
   return s;
+}
+
+static char* int2num(char *s, int x, int base, const char* table,
+                     char padding, int keta) {
+  int negative = FALSE;
+  if (x < 0) {
+    x = -x;
+    negative = TRUE;
+    --keta;
+  }
+  char* p = uint2num(s, x, base, table, padding, keta);
+  if (negative)
+    *(--p) = '-';
+  return p;
 }
 
 int toupper(int c) {
@@ -100,7 +105,7 @@ int vsprintf(char *str, const char *fmt, int* arg) {
 
     int keta = 0;
     char padding = ' ';
-    char buf[sizeof(int) * 3 + 1], *last = &buf[sizeof(buf)];
+    char buf[sizeof(int) * 3 + 3], *last = &buf[sizeof(buf)];
     char* q;
   again:
     switch (*(++fmt)) {
@@ -115,8 +120,13 @@ int vsprintf(char *str, const char *fmt, int* arg) {
       keta = *fmt - '0';
       goto again;
     case 'd': q = int2num(last, *arg++, 10, hextableLower, padding, keta); break;
-    case 'x': q = int2num(last, *arg++, 16, hextableLower, padding, keta); break;
-    case 'X': q = int2num(last, *arg++, 16, hextableUpper, padding, keta); break;
+    case 'x': q = uint2num(last, *arg++, 16, hextableLower, padding, keta); break;
+    case 'X': q = uint2num(last, *arg++, 16, hextableUpper, padding, keta); break;
+    case 'p':
+      q = uint2num(last, *arg++, 16, hextableLower, '0', sizeof(void*) * 2);
+      *(--q) = 'x';
+      *(--q) = '0';
+      break;
     }
     strcpy(dst, q);
     dst += (last - q) - 1;
