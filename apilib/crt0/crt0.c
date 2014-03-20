87944fcfa873a51804e97b14c60355a788705da4
tyfkda <tyfkda@gmail.com>
2014-03-20 17:25:56 +0900

    Specify data type directly

--- apilib/crt0/crt0.c
+++ apilib/crt0/crt0.c
@@ -4,8 +4,6 @@
 
 extern int main(int argc, char* argv[]);
 
-typedef void (*InitFunc)(void);
-
 /* These magic symbols are provided by the linker.  */
 extern void (*__preinit_array_start []) (void) __attribute__((weak));
 extern void (*__preinit_array_end []) (void) __attribute__((weak));
@@ -47,10 +45,10 @@ void HariMain(void) {
     *p++ = '\0';
   }
 
-  for (InitFunc* pp = __preinit_array_start; pp < __preinit_array_end; ++pp)
+  for (void (**pp)() = __preinit_array_start; pp < __preinit_array_end; ++pp)
     (**pp)();
   // TODO: Init.
-  for (InitFunc* pp = __init_array_start; pp < __init_array_end; ++pp)
+  for (void (**pp)() = __init_array_start; pp < __init_array_end; ++pp)
     (**pp)();
 
   int result = main(argc, argv);
